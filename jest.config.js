26de3e1c90f4e08148cbcf35b0ece90a47001934
Nervado <evandroabreudeoliveira@gmail.com>
2020-12-30 20:53:47 -0300

    refactor: change imports to use module

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,8 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
-
-/**
- *  //moduleNameMapper: {
-    //'@/(.*)': '<rootDir>/src/$1'
-  //}
-*/
