f77bcd953e696f3eb0c1d483f516889e0040efb3
Nervado <evandroabreudeoliveira@gmail.com>
2021-01-03 18:59:39 -0300

    feat: ensure AxiosHttpClient return correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -15,6 +15,13 @@ const mockPostRequest = (): HttpPostParams<any> => ({
   body: faker.random.objectElement()
 })
 
+const mockedResult = {
+  data: faker.random.objectElement(),
+  status: faker.random.number()
+}
+
+mockedAxios.post.mockResolvedValue(mockedResult)
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct URL and verb', async () => {
     const { url } = mockPostRequest()
@@ -28,5 +35,14 @@ describe('AxiosHttpClient', () => {
     const sut = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('Should return correct statusCode and body', async () => {
+    const sut = makeSut()
+    const result = await sut.post(mockPostRequest())
+    expect(result).toEqual({
+      statusCode: mockedResult.status,
+      body: mockedResult.data
+    })
   })
 })
