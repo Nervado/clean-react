d60232dc7b5cd98df1214a9d7cd15516b73e4290
Nervado <evandroabreudeoliveira@gmail.com>
2021-01-04 15:11:46 -0300

    refactor: separate all componentes off login view and create index file to components folder

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,36 +1,20 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import Logo from '@/presentation/components/logo/logo'
-import Spinner from '@/presentation/components/spinner/spinner'
+import { LoginHeader, Input, FormStatus, Footer } from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para Programadores</h1>
-      </header>
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" id="" placeholder="Digite seu email" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" id="" placeholder="Digite sua senha" />
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" id="" placeholder="Digite seu email" />
+        <Input type="password" name="password" id="" placeholder="Digite sua senha" />
         <button type="submit" className={Styles.submit}>Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-
-          <span className={Styles.error}>
-            Error
-          </span>
-        </div>
+        <FormStatus />
       </form>
-      <footer className={Styles.footer}></footer>
+      <Footer />
     </div>
   )
 }
