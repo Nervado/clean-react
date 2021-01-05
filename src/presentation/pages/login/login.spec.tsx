b7dc3a5bb14a1eaf767a58d6d38476268798a12e
Nervado <evandroabreudeoliveira@gmail.com>
2021-01-04 21:08:40 -0300

    test: ensure Login start with button submit disable

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,9 +4,11 @@ import Login from './login'
 // import Context from '@/presentation/contexts/form/form-context'
 
 describe('Login Component', () => {
-  test('', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
